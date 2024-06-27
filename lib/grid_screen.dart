11e66c2245a40b490a059bf17b7ad36610a17ce8
Lovejeetsingh2003 <135810389+Lovejeetsingh2003@users.noreply.github.com>
2024-06-27 15:29:00 +0530

    final commit

--- lib/grid_screen.dart
+++ lib/grid_screen.dart
@@ -10,103 +10,73 @@ class GridScreen extends StatefulWidget {
 class _GridScreenState extends State<GridScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: GridView.count(
-        crossAxisCount: 3,
-        children: [
-          GestureDetector(
-            child: Container(
-              color: Colors.grey,
-              child: Image(
-                  image: NetworkImage(
-                      "https://th.bing.com/th/id/OIP.8YcUU9ig_vlNnopAkLsiugHaLH?w=182&h=273&c=7&r=0&o=5&dpr=1.3&pid=1.7")),
-            ),
-            onTap: () {
-              Dialog(
-                child: StatefulBuilder(builder: (context, customState) {
-                  return Column(
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                      Container(
-                          child: Row(
-                        children: [
-                          Image(
-                            image: NetworkImage(
-                                "https://th.bing.com/th/id/OIP.8YcUU9ig_vlNnopAkLsiugHaLH?w=182&h=273&c=7&r=0&o=5&dpr=1.3&pid=1.7"),
-                            width: MediaQuery.of(context).size.width / 1.5,
-                            height: MediaQuery.of(context).size.height / 1.5,
-                          ),
-                          Row(
-                            children: [
-                              Icon(Icons.favorite),
-                              Icon(Icons.comment),
-                              Icon(Icons.mobile_screen_share_rounded),
-                            ],
-                          )
-                        ],
-                      )),
-                    ],
-                  );
-                }),
-              );
-            },
-          ),
-          Container(
-            color: Colors.green,
-            child: Image(
-                image: NetworkImage(
-                    "https://th.bing.com/th/id/OIP.0v1wZ86e3_S1KtZ9Ie7l4wHaLH?w=182&h=273&c=7&r=0&o=5&dpr=1.3&pid=1.7")),
-          ),
-          Container(
-            color: Colors.red.shade200,
-            child: Image(
-                image: NetworkImage(
-                    "https://th.bing.com/th/id/OIP.iZjJxjDbYbevgmlgJoOdVgHaLH?w=182&h=273&c=7&r=0&o=5&dpr=1.3&pid=1.7")),
-          ),
-          Container(
-            color: Colors.green,
-            child: Image(
-                image: NetworkImage(
-                    "https://th.bing.com/th/id/OIP.9aOUVEqDAH7xGGx99FTD2gHaKE?w=182&h=247&c=7&r=0&o=5&dpr=1.3&pid=1.7")),
-          ),
-          Container(
-            color: Colors.red.shade200,
-            child: Image(
-                image: NetworkImage(
-                    "https://th.bing.com/th/id/OIP.XPCAeXZXLMmkfaLB5BM6eQHaIl?w=182&h=211&c=7&r=0&o=5&dpr=1.3&pid=1.7")),
-          ),
-          Container(
-            color: Colors.grey,
-            child: Image(
-                image: NetworkImage(
-                    "https://th.bing.com/th/id/OIP.m4CQ7UrOBS13daqrzV8KYgHaKK?w=182&h=249&c=7&r=0&o=5&dpr=1.3&pid=1.7")),
-          ),
-          Container(
-            color: Colors.red.shade200,
-            child: Image(
-                image: NetworkImage(
-                    "https://th.bing.com/th/id/OIP.ByvyHuWKnCBT0K-AYFoWYAHaLH?w=182&h=273&c=7&r=0&o=5&dpr=1.3&pid=1.7")),
-          ),
-          Container(
-            color: Colors.grey,
-            child: Image(
-                image: NetworkImage(
-                    "https://th.bing.com/th/id/OIP.B6djh4tWcxtvLZBWRnuX-wHaLH?w=182&h=273&c=7&r=0&o=5&dpr=1.3&pid=1.7")),
-          ),
-          Container(
-            color: Colors.green,
-            child: Image(
-                image: NetworkImage(
-                    "https://th.bing.com/th/id/OIP.1gTb08i7y5_3ONerFk4WjwHaH9?w=182&h=196&c=7&r=0&o=5&dpr=1.3&pid=1.7")),
-          ),
-          Container(
-            color: Colors.grey,
-            child: Image(
-                image: NetworkImage(
-                    "https://th.bing.com/th/id/OIP.2TmIiSW5qgy_gW79C-ky2wHaNJ?w=182&h=324&c=7&r=0&o=5&dpr=1.3&pid=1.7")),
-          ),
-        ],
-      ),
+    return GridView.count(
+      shrinkWrap: true,
+      mainAxisSpacing: 3,
+      crossAxisSpacing: 3,
+      crossAxisCount: 3,
+      children: [
+        Container(
+          child: Image(
+              fit: BoxFit.fill,
+              image: NetworkImage(
+                  "https://th.bing.com/th/id/OIP.8YcUU9ig_vlNnopAkLsiugHaLH?w=182&h=273&c=7&r=0&o=5&dpr=1.3&pid=1.7")),
+        ),
+        Container(
+          child: Image(
+              fit: BoxFit.fill,
+              image: NetworkImage(
+                  "https://th.bing.com/th/id/OIP.0v1wZ86e3_S1KtZ9Ie7l4wHaLH?w=182&h=273&c=7&r=0&o=5&dpr=1.3&pid=1.7")),
+        ),
+        Container(
+          child: Image(
+              fit: BoxFit.fill,
+              image: NetworkImage(
+                  "https://th.bing.com/th/id/OIP.iZjJxjDbYbevgmlgJoOdVgHaLH?w=182&h=273&c=7&r=0&o=5&dpr=1.3&pid=1.7")),
+        ),
+        Container(
+          child: Image(
+              fit: BoxFit.fill,
+              image: NetworkImage(
+                  "https://th.bing.com/th/id/OIP.9aOUVEqDAH7xGGx99FTD2gHaKE?w=182&h=247&c=7&r=0&o=5&dpr=1.3&pid=1.7")),
+        ),
+        Container(
+          child: Image(
+              fit: BoxFit.fill,
+              image: NetworkImage(
+                  "https://th.bing.com/th/id/OIP.XPCAeXZXLMmkfaLB5BM6eQHaIl?w=182&h=211&c=7&r=0&o=5&dpr=1.3&pid=1.7")),
+        ),
+        Container(
+          child: Image(
+              fit: BoxFit.fill,
+              image: NetworkImage(
+                  "https://th.bing.com/th/id/OIP.m4CQ7UrOBS13daqrzV8KYgHaKK?w=182&h=249&c=7&r=0&o=5&dpr=1.3&pid=1.7")),
+        ),
+        Container(
+          child: Image(
+              fit: BoxFit.fill,
+              image: NetworkImage(
+                  "https://th.bing.com/th/id/OIP.ByvyHuWKnCBT0K-AYFoWYAHaLH?w=182&h=273&c=7&r=0&o=5&dpr=1.3&pid=1.7")),
+        ),
+        Container(
+          child: Image(
+              fit: BoxFit.fill,
+              image: NetworkImage(
+                  "https://th.bing.com/th/id/OIP.B6djh4tWcxtvLZBWRnuX-wHaLH?w=182&h=273&c=7&r=0&o=5&dpr=1.3&pid=1.7")),
+        ),
+        Container(
+          child: Image(
+              fit: BoxFit.fill,
+              image: NetworkImage(
+                  "https://th.bing.com/th/id/OIP.1gTb08i7y5_3ONerFk4WjwHaH9?w=182&h=196&c=7&r=0&o=5&dpr=1.3&pid=1.7")),
+        ),
+        Container(
+          child: Image(
+              fit: BoxFit.fill,
+              image: NetworkImage(
+                  "https://th.bing.com/th/id/OIP.2TmIiSW5qgy_gW79C-ky2wHaNJ?w=182&h=324&c=7&r=0&o=5&dpr=1.3&pid=1.7")),
+        ),
+      ],
     );
   }
 }
--- lib/insta_page.dart
+++ lib/insta_page.dart
@@ -13,187 +13,189 @@ class _InstaPageState extends State<InstaPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
-      body: Container(
-        margin: const EdgeInsets.fromLTRB(20, 0, 20, 0),
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                const Row(
-                  children: [
-                    Text(
-                      "Username",
-                      style: TextStyle(
-                        fontSize: 30,
-                        fontWeight: FontWeight.w500,
-                        color: Colors.black,
-                      ),
-                    ),
-                    Padding(
-                      padding: EdgeInsets.all(10),
-                      child: Icon(
-                        Icons.arrow_drop_down_rounded,
-                        size: 30,
-                        color: Colors.black,
-                      ),
-                    ),
-                  ],
-                ),
-                Row(
-                  children: [
-                    const Padding(
-                      padding: EdgeInsets.all(20),
-                      child: Icon(
-                        Icons.add_box_outlined,
-                        size: 30,
-                        color: Colors.black,
-                      ),
-                    ),
-                    PopupMenuButton(
-                      icon: const Icon(
-                        Icons.menu,
-                        size: 30,
-                        color: Colors.black,
-                      ),
-                      itemBuilder: (context) {
-                        return [
-                          const PopupMenuItem(
-                            child: Text("First"),
-                          ),
-                          const PopupMenuItem(
-                            child: Text("Second"),
-                          ),
-                        ];
-                      },
-                    ),
-                  ],
-                ),
-              ],
-            ),
-            Container(
-              child: const Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      body: SingleChildScrollView(
+        child: Container(
+          margin: const EdgeInsets.fromLTRB(20, 0, 20, 0),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
-                  CircleAvatar(
-                    radius: 40,
-                    backgroundImage: AssetImage("assets/images/image.jpeg"),
-                  ),
-                  Column(
+                  const Row(
                     children: [
                       Text(
-                        "574",
+                        "Username",
                         style: TextStyle(
-                          fontSize: 20,
-                          fontWeight: FontWeight.bold,
+                          fontSize: 30,
+                          fontWeight: FontWeight.w500,
                           color: Colors.black,
                         ),
                       ),
-                      Text(
-                        "Posts",
-                        style: TextStyle(
-                          fontSize: 16,
-                          fontWeight: FontWeight.w500,
+                      Padding(
+                        padding: EdgeInsets.all(10),
+                        child: Icon(
+                          Icons.arrow_drop_down_rounded,
+                          size: 30,
                           color: Colors.black,
                         ),
                       ),
                     ],
                   ),
-                  Column(
+                  Row(
                     children: [
-                      Text(
-                        "1,080",
-                        style: TextStyle(
-                          fontSize: 20,
-                          fontWeight: FontWeight.bold,
+                      const Padding(
+                        padding: EdgeInsets.all(20),
+                        child: Icon(
+                          Icons.add_box_outlined,
+                          size: 30,
                           color: Colors.black,
                         ),
                       ),
-                      Text(
-                        "Followers",
-                        style: TextStyle(
-                          fontSize: 16,
-                          fontWeight: FontWeight.w500,
+                      PopupMenuButton(
+                        icon: const Icon(
+                          Icons.menu,
+                          size: 30,
                           color: Colors.black,
                         ),
-                      ),
-                    ],
-                  ),
-                  Column(
-                    children: [
-                      Text(
-                        "712",
-                        style: TextStyle(
-                          fontSize: 20,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.black,
-                        ),
-                      ),
-                      Text(
-                        "Following",
-                        style: TextStyle(
-                          fontSize: 16,
-                          fontWeight: FontWeight.w500,
-                          color: Colors.black,
-                        ),
+                        itemBuilder: (context) {
+                          return [
+                            const PopupMenuItem(
+                              child: Text("First"),
+                            ),
+                            const PopupMenuItem(
+                              child: Text("Second"),
+                            ),
+                          ];
+                        },
                       ),
                     ],
                   ),
                 ],
               ),
-            ),
-            const Row(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: [
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
+              Container(
+                child: const Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                   children: [
-                    Text(
-                      "Username",
-                      textAlign: TextAlign.start,
-                      style: TextStyle(
-                          fontSize: 28,
-                          fontWeight: FontWeight.w500,
-                          color: Colors.black),
+                    CircleAvatar(
+                      radius: 40,
+                      backgroundImage: AssetImage("assets/images/image.jpeg"),
                     ),
-                    Text(
-                      "From Italy Living in California",
-                      textAlign: TextAlign.start,
-                      style: TextStyle(
-                          fontSize: 20,
-                          fontWeight: FontWeight.w500,
-                          color: Colors.black),
+                    Column(
+                      children: [
+                        Text(
+                          "574",
+                          style: TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.black,
+                          ),
+                        ),
+                        Text(
+                          "Posts",
+                          style: TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.w500,
+                            color: Colors.black,
+                          ),
+                        ),
+                      ],
+                    ),
+                    Column(
+                      children: [
+                        Text(
+                          "1,080",
+                          style: TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.black,
+                          ),
+                        ),
+                        Text(
+                          "Followers",
+                          style: TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.w500,
+                            color: Colors.black,
+                          ),
+                        ),
+                      ],
+                    ),
+                    Column(
+                      children: [
+                        Text(
+                          "712",
+                          style: TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.black,
+                          ),
+                        ),
+                        Text(
+                          "Following",
+                          style: TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.w500,
+                            color: Colors.black,
+                          ),
+                        ),
+                      ],
                     ),
                   ],
                 ),
-              ],
-            ),
-            Container(
-              margin: EdgeInsets.fromLTRB(0, 20, 0, 10),
-              child: const Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              ),
+              const Row(
+                mainAxisAlignment: MainAxisAlignment.start,
                 children: [
-                  Icon(
-                    Icons.grid_on_outlined,
-                    size: 50,
-                    color: Colors.black,
-                  ),
-                  Icon(
-                    Icons.video_camera_back_outlined,
-                    size: 50,
-                    color: Colors.black,
-                  ),
-                  Icon(
-                    Icons.assignment_ind,
-                    size: 50,
-                    color: Colors.black,
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "Username",
+                        textAlign: TextAlign.start,
+                        style: TextStyle(
+                            fontSize: 28,
+                            fontWeight: FontWeight.w500,
+                            color: Colors.black),
+                      ),
+                      Text(
+                        "From Italy Living in California",
+                        textAlign: TextAlign.start,
+                        style: TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.w500,
+                            color: Colors.black),
+                      ),
+                    ],
                   ),
                 ],
               ),
-            ),
-            const Expanded(child: GridScreen())
-          ],
+              Container(
+                margin: EdgeInsets.fromLTRB(0, 20, 0, 10),
+                child: const Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    Icon(
+                      Icons.grid_on_outlined,
+                      size: 50,
+                      color: Colors.black,
+                    ),
+                    Icon(
+                      Icons.video_camera_back_outlined,
+                      size: 50,
+                      color: Colors.black,
+                    ),
+                    Icon(
+                      Icons.assignment_ind,
+                      size: 50,
+                      color: Colors.black,
+                    ),
+                  ],
+                ),
+              ),
+              GridScreen()
+            ],
+          ),
         ),
       ),
       bottomNavigationBar: const BottomAppBar(
